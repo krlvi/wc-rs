fb7557724793bb901e5354789bb6f657aaa4229b
Kiril Videlov <kiril.vi@runbox.no>
2019-05-08 22:12:59 +0200

    Loading of the words map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
+
+fn load_dict(dict_file: String) -> HashMap<String, i32> {
+    let mut dict : HashMap<String, i32> = HashMap::new();
+
+    match File::open(dict_file) {
+        Ok(file) => {
+            for line in BufReader::new(file).lines() {
+                match line {
+                    Ok(line) => {
+                        dict.insert(line, 1);
+                    },
+                    Err(error) => {
+                        panic!("There was a problem reading a line: {:?}", error)
+                    }
+                }
+            }
+        },
+        Err(error) => {
+            panic!("There was a problem opening the file: {:?}", error)
+        }
+    }
+    dict
+}
 fn main() {
-    println!("Hello, world!");
+    let dict = load_dict(String::from("/home/kiril/tmp/wordcounting/words_alpha.txt"));
+    println!("{}", dict.len());
 }
