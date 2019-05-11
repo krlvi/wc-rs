abf33f5148245103f30eb5987b73cb35115eeef0
Kiril Videlov <kiril.vi@runbox.no>
2019-05-11 14:33:40 +0200

    Sorting in place

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn load_dict(dict_file: String) -> HashMap<String, i32> {
             for line in BufReader::new(file).lines() {
                 match line {
                     Ok(line) => {
-                        dict.insert(line, 1);
+                        dict.insert(line, 0);
                     },
                     Err(error) => {
                         panic!("There was a problem reading a line: {:?}", error)
@@ -24,7 +24,44 @@ fn load_dict(dict_file: String) -> HashMap<String, i32> {
     }
     dict
 }
+
+fn count_words(dict: &mut HashMap<String, i32>, input_file: String) {
+    match File::open(input_file) {
+        Ok(file) => {
+            for line in BufReader::new(file).lines() {
+                match line {
+                    Ok(line) => {
+                        match dict.get(&line) {
+                            Some(val) => {
+                                dict.insert(line, *val + 1);
+                            },
+                            None => {}
+                        }
+                    },
+                    Err(error) => {
+                        panic!("there was a problem reading a line: {:?}", error)
+                    }
+                }
+            }
+
+        },
+        Err(error) => {
+            panic!("There was a problem opening the input file {:?},", error)
+        }
+    }
+}
 fn main() {
-    let dict = load_dict(String::from("/home/kiril/tmp/wordcounting/words_alpha.txt"));
-    println!("{}", dict.len());
+    let mut dict = load_dict(String::from("/home/kiril/tmp/wordcounting/words_alpha.txt"));
+    count_words(&mut dict, String::from("/home/kiril/tmp/wordcounting/test.txt"));
+
+    let mut res: Vec<(&String, &i32)> = dict.iter()
+        .filter(|(_k, v)| v.is_positive())
+        .map(|(k, v)| (k, v))
+        .collect();
+    res.sort_by(|(_, xv), (_, yv)| yv.cmp(xv));
+    res.truncate(10);
+
+    for (k, v) in res {
+        println!("{} {}", k, v);
+    }
 }
