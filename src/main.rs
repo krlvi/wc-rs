c678026dcceacb4ae2575d020d1f77f65e13701a
Kiril Videlov <kiril.vi@runbox.no>
2019-05-12 14:53:44 +0200

    a working solution now

--- src/main.rs
+++ src/main.rs
@@ -12,28 +12,6 @@ fn load_dict(dict_file: &String) -> HashMap<String, i32> {
     dict
 }
 
-fn count_words(dict: &mut HashMap<String, i32>, input_file: String) {
-    match File::open(input_file) {
-        Ok(file) => {
-            for line in BufReader::new(file).lines() {
-                match line {
-                    Ok(line) => {
-                        for word in line.split_whitespace() {
-                            match dict.get(word) {
-                                Some(val) => {
-                                    dict.insert(String::from(word), *val + 1);
-                                }
-                                None => {}
-                            }
-                        }
-                    }
-                    Err(error) => panic!("there was a problem reading a line: {:?}", error),
-                }
-            }
-        }
-        Err(error) => panic!("There was a problem opening the input file {:?},", error),
-    }
-}
 fn lines(file: &String) -> std::io::Lines<std::io::BufReader<std::fs::File>> {
     match File::open(file) {
         Ok(file) => BufReader::new(file).lines(),
@@ -41,7 +19,7 @@ fn lines(file: &String) -> std::io::Lines<std::io::BufReader<std::fs::File>> {
     }
 }
 
-fn work(rx: Receiver<Option<String>>, mut dict: HashMap<String, i32>) -> HashMap<String, i32> {
+fn work(rx: Receiver<Option<String>>, mut dict: HashMap<String, i32>) -> Vec<(String, i32)> {
     loop {
         for msg in rx.recv().iter() {
             match msg {
@@ -56,7 +34,16 @@ fn work(rx: Receiver<Option<String>>, mut dict: HashMap<String, i32>) -> HashMap
                     }
                 }
                 None => {
-                    return dict;
+                    let mut res: Vec<(&String, &i32)> =
+                        dict.iter().filter(|(_k, v)| v.is_positive()).collect();
+                    res.sort_by(|(_, xv), (_, yv)| yv.cmp(xv));
+                    res.truncate(10);
+
+                    let mut out = Vec::new();
+                    for (k, v) in res {
+                        out.push((k.clone(), v.clone()));
+                    }
+                    return out;
                 }
             }
         }
@@ -64,19 +51,6 @@ fn work(rx: Receiver<Option<String>>, mut dict: HashMap<String, i32>) -> HashMap
 }
 
 fn main() {
-    //    let dict = load_dict(String::from("/home/kiril/tmp/wordcounting/words_alpha.txt"));
-    // count_words(&mut dict, small);
-    //    count_words(&mut dict, String::from("/home/kiril/tmp/wordcounting/14m_hn_comments_sorted.txt"));
-
-    /*
-    let mut res: Vec<(&String, &i32)> = dict.iter()
-        .filter(|(_k, v)| v.is_positive())
-      //  .map(|(k, v)| (k, v))
-        .collect();
-    res.sort_by(|(_, xv), (_, yv)| yv.cmp(xv));
-    res.truncate(10);
-    */
-
     let dict_file = String::from("/home/kiril/tmp/wordcounting/words_alpha.txt");
     let input_file = String::from("/home/kiril/tmp/wordcounting/small.txt");
 
@@ -107,7 +81,26 @@ fn main() {
     }
 
     // Wait for threads to finish and print their local result
+    // let mut res: Vec<(String, i32)> = Vec::new();
+    let mut re: HashMap<String, i32> = HashMap::new();
     for handle in handles {
-        println!("{:?}", handle.join().unwrap().len());
+        let thr_res = handle.join().unwrap();
+        for (k, v) in thr_res {
+            match re.get(&k) {
+                Some(ev) => {
+                    re.insert(k, ev + v);
+                }
+                None => {
+                    re.insert(k, v);
+                }
+            }
+        }
+    }
+    let mut res: Vec<(&String, &i32)> = re.iter().collect();
+    res.sort_by(|(_, xv), (_, yv)| yv.cmp(xv));
+    res.truncate(10);
+
+    for (k, v) in res {
+        println!("{} {}", k, v);
     }
 }
